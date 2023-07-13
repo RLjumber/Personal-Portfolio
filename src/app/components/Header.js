66be0b2b0194483bb1d12fb1a400d3c3227d1007
RLjumber <ronaldjumber@gmail.com>
2023-07-13 15:28:49 -0400

    Header sticky to top, width full, next step is to create hamburger for menu when on mobile first

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -7,11 +7,13 @@ export default function Header() {
     const pathname = usePathname();
     
     return (
-            <nav>
+            <nav class="w-full flex sticky top-0 grid grid-cols-2 gap-4">
                 <a href="/" class="hover:text-emerald-800">R. Jumber</a>
-                {pathname === "/" ? <a href="/" class="text-emerald-500 hover:text-emerald-500">Work</a> : <a href="/" class="hover:text-emerald-500">Work</a>}
-                {pathname === "/about" ? <a href="/about" class="text-emerald-500 hover:text-emerald-500">About</a> : <a href="/about" class="hover:text-emerald-500">About</a>}
-                {pathname === "/contact" ? <a href="/contact" class="text-emerald-500 hover:text-emerald-500">Contact</a> : <a href="/contact" class="hover:text-emerald-500">Contact</a>}
+                <div class="">
+                    {pathname === "/" ? <a href="/" class="text-emerald-500 hover:text-emerald-500">Work</a> : <a href="/" class="hover:text-emerald-500">Work</a>}
+                    {pathname === "/about" ? <a href="/about" class="text-emerald-500 hover:text-emerald-500">About</a> : <a href="/about" class="hover:text-emerald-500">About</a>}
+                    {pathname === "/contact" ? <a href="/contact" class="text-emerald-500 hover:text-emerald-500">Contact</a> : <a href="/contact" class="hover:text-emerald-500">Contact</a>}
+                </div>
             </nav>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,10 @@ import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <main>
+    <main >
       <Header />
-      <h1>Home</h1>
+      <h1 class="font-display" >Hi, I`m Luke</h1>
+      <h2>(Frontend, Fullstack, UI/UX, Design)</h2>
     </main>
   )
 }
