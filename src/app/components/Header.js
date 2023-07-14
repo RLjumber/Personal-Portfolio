4cd33f312a0adfdffaf57ca5e5beace137c3b337
RLjumber <ronaldjumber@gmail.com>
2023-07-13 20:39:07 -0400

    Style:Font size and borders

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -5,15 +5,15 @@ import { usePathname } from "next/navigation";
 export default function Header() {
 
     const pathname = usePathname();
-    
+
     return (
-            <nav class="w-full flex sticky top-0 grid grid-cols-2 gap-4">
-                <a href="/" class="hover:text-emerald-800">R. Jumber</a>
-                <div class="">
-                    {pathname === "/" ? <a href="/" class="text-emerald-500 hover:text-emerald-500">Work</a> : <a href="/" class="hover:text-emerald-500">Work</a>}
-                    {pathname === "/about" ? <a href="/about" class="text-emerald-500 hover:text-emerald-500">About</a> : <a href="/about" class="hover:text-emerald-500">About</a>}
-                    {pathname === "/contact" ? <a href="/contact" class="text-emerald-500 hover:text-emerald-500">Contact</a> : <a href="/contact" class="hover:text-emerald-500">Contact</a>}
-                </div>
-            </nav>
+        <nav class="w-full sticky top-0 grid grid-cols-2 gap-4 border">
+            <a href="/" class="hover:text-emerald-800">R. Jumber</a>
+            <div>
+                {pathname === "/" ? <a href="/" class="text-emerald-500 hover:text-emerald-500">Work</a> : <a href="/" class="hover:text-emerald-500">Work</a>}
+                {pathname === "/about" ? <a href="/about" class="text-emerald-500 hover:text-emerald-500">About</a> : <a href="/about" class="hover:text-emerald-500">About</a>}
+                {pathname === "/contact" ? <a href="/contact" class="text-emerald-500 hover:text-emerald-500">Contact</a> : <a href="/contact" class="hover:text-emerald-500">Contact</a>}
+            </div>
+        </nav>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,10 @@ import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <main >
+    <main class="container px-3 py-2">
       <Header />
-      <h1 class="font-display" >Hi, I`m Luke</h1>
-      <h2>(Frontend, Fullstack, UI/UX, Design)</h2>
+      <h1 class="font-display border-red-500 text-9xl" >Hi, I`m Luke</h1>
+      <h2 class="font-thin text-4xl">(Frontend, Fullstack, UI/UX, Design)</h2>
     </main>
   )
 }
