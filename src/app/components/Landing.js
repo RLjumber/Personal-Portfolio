39e71503fbb241fb1b84e4c255eb472d9ae2adaf
RLjumber <ronaldjumber@gmail.com>
2023-10-30 23:58:01 -0400

    UPDATE: Getting back into it, small changes to structure. keeping main page.js to calling components, Landing component acts as home content now.

---
+++ src/app/components/Landing.js
@@ -0,0 +1,16 @@
+export default function Landing() {
+    return (
+        <section class="border border-green-400">
+
+            <div class="h-screen flex flex-col justify-center pb-10">
+                <h1 class="font-display text-9xl" >Hi, I`m Luke</h1>
+                <h2 class="font-thin text-4xl">Frontend, Fullstack, UI/UX, Design</h2>
+            </div>
+
+            <div class="">
+                <h3>Content here! Not sure why putting this here messed up the centering of the above content</h3>
+            </div>
+        
+      </section>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,11 @@
 import Header from "./components/Header";
+import Landing from "./components/Landing";
 
 export default function Home() {
   return (
     <main class="px-3 py-2 w-full">
       <Header />
-      <div class="border border-green-400">
-
-        <div class="h-screen flex flex-col justify-center pb-10">
-          <h1 class="font-display text-9xl" >Hi, I`m Luke</h1>
-          <h2 class="font-thin text-4xl">Frontend, Fullstack, UI/UX, Design</h2>
-        </div>
-
-        <div>
-          <h3>More content here! Not sure why putting this here messed up the centering of the above content</h3>
-        </div>
-        
-      </div>
+      <Landing />
     </main>
   )
 }
