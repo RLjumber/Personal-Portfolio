b9cae33a760595ae0be735ad3c86fc19b4446d30
RLjumber <ronaldjumber@gmail.com>
2023-11-07 14:11:42 -0500

    LEFTOVERS: nothing important

--- src/app/components/Landing.js
+++ src/app/components/Landing.js
@@ -7,17 +7,17 @@ export default function Landing() {
                     <h2 class=" font-light text-4xl">Welcome To My Digital Portfolio</h2>
                 </div>
             </section>
-            <section id="who-and-what" class="border-t-2 pt-10">
+            <section id="who-and-what" class="border-t-2 border pt-10">
                 <div class="flex flex-col">
-                    <h3 class="font-display text-5xl">What I Do</h3>
-                    <ul class="flex flex-col h-full items-end text-5xl font-light justify-evenly">
-                        <li><h3 class="">Frontend</h3></li>
-                        <li><h3 class="">Fullstack</h3></li>
-                        <li><h3 class="">Design</h3></li>
+                    <h3 class="font-display text-5xl pb-10">What I Do</h3>
+                    <ul class="flex flex-col h-full w-full items-end text-5xl font-light justify-evenly">
+                        <li><h3 class="border p-5 hover:bg-red-400 w-full">Frontend</h3></li>
+                        <li><h3 class="p-5">Fullstack</h3></li>
+                        <li><h3 class="p-5">Design</h3></li>
                     </ul>
                 </div>
                 <div class="font-display text-5xl pt-10">
-                    <h3>Who I am</h3>
+                    <h3 class="pb-10">Who I am</h3>
                 </div>
             </section>
       </main>
